3ffe320acef90495d193511f60ff0f0a3baf18d1
andredomin <andres10dominguez@gmail.com>
2024-01-29 16:40:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>
+      <div className="global">
+        <div className="title"><h1>SmartSpendr</h1></div>
+      <div className="ahorro"><h2><a href="https://control-ahorro.netlify.app/">Meta de ahorro</a></h2></div>
+      <div className="gastos"><h2><a href='https://panelgastos.netlify.app/'>Control de gastos</a></h2></div>
+      <div className="footer"></div>
+      </div>
+    </h1>
+    </>
+  )
+}
+
+export default App
